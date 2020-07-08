817daf47cda1a4051060360e1a33256b8eb401a5
gaurangparmar89 <gaurangparmar1989@gmail.com>
2020-07-08 09:07:10 -0500

    adding the file in js-assign branch

---
+++ parachange.js
@@ -0,0 +1,10 @@
+function change(){
+	
+	paraReference=
+		document.getElementById("para1");
+	
+	//document.write("this is what is written : " + 
+		//	paraReference.innerHTML);
+	
+	paraReference.innerHTML="new vlaue";
+}
